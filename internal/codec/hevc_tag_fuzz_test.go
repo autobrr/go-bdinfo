a4921dd14d4f5560d86d0d0aef99332eb79f57ff
soup <s0up4200@pm.me>
2026-03-14 17:44:35 +0100

    test: cap HEVC tag fuzz payload size

--- internal/codec/hevc_tag_fuzz_test.go
+++ internal/codec/hevc_tag_fuzz_test.go
@@ -8,7 +8,9 @@ func FuzzHEVCFrameTagFromTransfer(f *testing.F) {
 	f.Add([]byte{0x00, 0x00, 0x01, 0x44, 0x01, 0x01, 0x01}) // PPS-ish
 
 	f.Fuzz(func(t *testing.T, data []byte) {
-		if len(data) > 2<<20 {
+		// Per-transfer HEVC tag parsing only needs small Annex-B payloads; cap
+		// fuzz inputs to avoid CI timeout flakes on oversized pathological cases.
+		if len(data) > 256<<10 {
 			return
 		}
 		state := &HEVCTagState{}
